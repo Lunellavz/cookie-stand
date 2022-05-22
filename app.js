2de533c567ba187ad0e2729a52e2d1ee2336eabf
Gabriella <gabi.andino2000@gmail.com>
2022-05-21 17:30:08 -0700

    staring cookie stand

---
+++ app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let storeHours = [
+'6am',
+'7am', 
+'8am', 
+'9am', 
+'10am',
+'11am', 
+'12pm', 
+'1pm', 
+'2pm', 
+'3pm', 
+'4pm', 
+'5pm',
+'6pm', 
+'7pm', 
+'8pm',
+];
+
+
+let cookieStores = [
+    {
+        location: 'Seattle',
+        minCust: 23,
+        maxCust: 65,
+        avgCookies: 6.3,
+    },
+    {
+        location: 'Tokyo',
+        minCust: 3,
+        maxCust: 24,
+        avgCookies: 1.2,
+    },
+    {
+        location: 'Dubai',
+        minCust: 11,
+        maxCust: 38,
+        avgCookies: 3.7,
+    },
+    {
+        location: 'Paris',
+        minCust: 20,
+        maxcust:38,
+        avgCookies: 2.3,
+    },
+    {
+        location: 'Lima',
+        minCust: 2,
+        maxCust: 16,
+        avgCookies: 4.6,
+    },
+]
